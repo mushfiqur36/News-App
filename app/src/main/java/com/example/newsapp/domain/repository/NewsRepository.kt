bf333155b52c4b26ad3ab8fcb7f0dfa20afc5b5c
mushfiqur36 <rahmanmushfiqur167@gmail.com>
2024-03-07 19:44:38 +0100

    added remote data source and implemented related functions

--- app/src/main/java/com/example/newsapp/domain/repository/NewsRepository.kt
+++ app/src/main/java/com/example/newsapp/domain/repository/NewsRepository.kt
@@ -1,14 +1,14 @@
 package com.example.newsapp.domain.repository
 
-import com.example.newsapp.data.model.ApiResponse
+import com.example.newsapp.data.model.APIResponse
 import com.example.newsapp.data.model.Article
 import com.example.newsapp.data.util.Resource
 import kotlinx.coroutines.flow.Flow
 
 interface NewsRepository {
 
-    suspend fun getNewsHeadlines(): Resource<ApiResponse>
-    suspend fun getSearchedNews(searchQuery: String): Resource<ApiResponse>
+    suspend fun getNewsHeadlines(): Resource<APIResponse>
+    suspend fun getSearchedNews(searchQuery: String): Resource<APIResponse>
     suspend fun saveNews(article: Article)
     suspend fun deleteNews(article: Article)
     fun getSavedNews(): Flow<List<Article>>
