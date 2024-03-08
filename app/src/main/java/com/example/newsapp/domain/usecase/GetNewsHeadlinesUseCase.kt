ef5593dfb2edae200823161e127db49d8be4d3b5
mushfiqur36 <rahmanmushfiqur167@gmail.com>
2024-03-08 11:29:28 +0100

    refactor repositories with country and page parameter

--- app/src/main/java/com/example/newsapp/domain/usecase/GetNewsHeadlinesUseCase.kt
+++ app/src/main/java/com/example/newsapp/domain/usecase/GetNewsHeadlinesUseCase.kt
@@ -5,7 +5,7 @@ import com.example.newsapp.data.util.Resource
 import com.example.newsapp.domain.repository.NewsRepository
 
 class GetNewsHeadlinesUseCase(private val newsRepository: NewsRepository) {
-    suspend fun execute(): Resource<APIResponse> {
-        return newsRepository.getNewsHeadlines()
+    suspend fun execute(country: String, page: Int): Resource<APIResponse> {
+        return newsRepository.getNewsHeadlines(country, page)
     }
 }
